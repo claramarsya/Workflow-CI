c3108437237dfc0d4f1c2deaba3d040f1700063c
claramarsya <claramarsyaa@gmail.com>
2025-11-26 20:20:11 +0700

    modified

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,38 +7,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score, classification_report
-import uvicorn
-
+from sklearn.metrics import accuracy_score, classification_report, f1_score
 
 def train_model(data_path):
-    print("=== MEMUAT DATASET PREPROCESSING ===")
     df = pd.read_csv(data_path)
-    print(df.head())
     
     text_col = "clean_title"
     label_col = "real"
     
-    # === SAFETY CLEANING (FIX ERROR) ===
-    print("\n=== FIXING NaN / BAD STRINGS ===")
-    
-    # pastikan kolom ada
     if text_col not in df.columns:
         raise Exception(f"Kolom '{text_col}' tidak ditemukan! Apakah file hasil preprocessing benar?")
     
-    # drop baris dengan missing text
     df = df.dropna(subset=[text_col])
     
-    # convert ke string
     df[text_col] = df[text_col].astype(str)
     
-    # ganti string kosong
     df[text_col] = df[text_col].replace("", "empty")
     
-    print("Sisa data setelah cleaning:", len(df))
-
-
-    print("\n=== SPLIT DATA ===")
     X_train, X_test, y_train, y_test = train_test_split(
         df[text_col],
         df[label_col],
@@ -46,35 +31,34 @@ def train_model(data_path):
         random_state=42,
         stratify=df[label_col]
     )
-    print("Train size:", len(X_train))
-    print("Test size :", len(X_test))
 
-    # MLflow Setup
     mlflow.set_experiment("FakeNews_Clara")
 
     with mlflow.start_run():
-        # Model pipeline
+        mlflow.log_param("max_features", 5000)
+        mlflow.log_param("classifier", "LogisticRegression")
+        mlflow.log_param("class_weight", "balanced")
+
         pipeline = Pipeline([
             ('tfidf', TfidfVectorizer(max_features=5000)),
-            ('clf', LogisticRegression(max_iter=200))
+            ('clf', LogisticRegression(
+                max_iter=200, 
+                class_weight='balanced',
+                solver='liblinear'
+            ))
         ])
 
-        print("\n=== TRAINING MODEL ===")
         pipeline.fit(X_train, y_train)
 
-        print("\n=== EVALUASI MODEL ===")
         y_pred = pipeline.predict(X_test)
+        
         acc = accuracy_score(y_test, y_pred)
-        print("Accuracy:", acc)
-        print(classification_report(y_test, y_pred))
-
-        # Log metrics ke MLflow
+        f1_weighted = f1_score(y_test, y_pred, average='weighted')
+        
         mlflow.log_metric("accuracy", acc)
+        mlflow.log_metric("f1_weighted", f1_weighted)
 
-        # Log model
         mlflow.sklearn.log_model(pipeline, artifact_path="model")
-
-        print("\nModel berhasil disimpan ke MLflow.")
 
     return pipeline
 
